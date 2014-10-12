68ace70a20800798a41fb7e12b8c1bc966eda69b
Jake Kerr <kodafox@gmail.com>
2014-10-12 19:55:51 +0900

    Remove silly code from systems

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,7 @@ extern crate piston;
 extern crate sdl2_game_window;
 extern crate opengl_graphics;
 use std::cell::RefCell;
+use std::default::Default;
 use std::rc::Rc;
 use std::rand;
 use std::rand::Rng;
@@ -24,6 +25,7 @@ use piston::{
 use piston::graphics::{
     AddBorder,
     AddRectangle,
+    AddEllipse,
     AddColor,
     Context,
     Draw,
@@ -61,7 +63,7 @@ impl DrawSystem {
         let opengl = piston::shader_version::opengl::OpenGL_3_2;
         DrawSystem {
             gl: RefCell::new(Gl::new(opengl)),
-            event: event 
+            event: event
         }
     }
 }
@@ -78,23 +80,56 @@ impl BulkEntityProcess for DrawSystem {
         // Clear background.
         c.rgb(0.0, 0.0, 0.0).draw(gl);
 
-        let halfw = w / 2.0;
-        let halfh = h / 2.0;
         for e in es.iter() {
-          if let Some(ref mut position) = data.borrow::<Position>(*e) {
-            let dx = halfw - position.x;
-            let dy = halfh - position.y;
-            let dx2 = dx * dx;
-            let dy2 = dy * dy;
-            let maxd2 = halfw * halfw + halfh * halfh;
-            let d2 = dx2 + dy2;
-            let rad = position.rad * (1.0 - d2 / maxd2);
-            let ref mut rng = rand::task_rng();
-            let l = rng.gen_range(0.8, 1.0);
-            c.rgb(0.5 * l, 0.2 * l, 0.8 * l)
-              .rect(position.x, position.y, rad, rad)
-              .border_width(rng.gen_range(1.0, 3.0))
-              .draw(gl);
+          let mut x: f64 = 0.0;
+          let mut y: f64 = 0.0;
+          let mut shape = Point;
+          let mut border = None;
+          if let Some(p) = data.borrow::<PositionComponent>(*e) {
+            x = p.x;
+            y = p.y;
+          }
+          if let Some(s) = data.borrow::<ShapeComponent>(*e) {
+            shape = s.shape;
+            border = s.border;
+          }
+          let r = 0.5;
+          let g = 0.2;
+          let b = 0.8;
+          match (shape, border) {
+              (Point, Some(border)) => {
+                  c.rgb(r, g, b)
+                    .rect(x, y, 1.0, 1.0)
+                    .border_radius(border)
+                    .draw(gl);
+              },
+              (Circle(rad), Some(border)) => {
+                  c.rgb(r, g, b)
+                    .ellipse(x, y, rad, rad)
+                    .border_radius(border)
+                    .draw(gl);
+              },
+              (Square(w,h), Some(border)) => {
+                  c.rgb(r, g, b)
+                    .rect(x, y, w, h)
+                    .border_radius(border)
+                    .draw(gl);
+              },
+              (Point, None) => {
+                  c.rgb(r, g, b)
+                    .rect(x, y, 1.0, 1.0)
+                    .draw(gl);
+              },
+              (Circle(rad), None) => {
+                  c.rgb(r, g, b)
+                    .ellipse(x, y, rad, rad)
+                    .draw(gl);
+              },
+              (Square(w,h), None) => {
+                  c.rgb(r, g, b)
+                    .rect(x, y, w, h)
+                    .draw(gl);
+              }
           }
         }
     }
@@ -108,7 +143,7 @@ struct MoveSystem {
 impl MoveSystem {
   fn new(event: Rc<RefCell<Event>>) -> MoveSystem {
     MoveSystem {
-        event: event 
+        event: event
     }
   }
 }
@@ -120,20 +155,13 @@ impl BulkEntityProcess for MoveSystem {
         for e in es.iter() {
           let xv;
           let yv;
-          if let Some(ref mut velocity) = data.borrow::<Velocity>(*e) {
-              let ref mut rng = rand::task_rng();
-              if rng.gen_range(0u, 100u) >= 99 {
-                //velocity.y = -velocity.y;
-              }
-              if rng.gen_range(0u, 100u) >= 99 {
-                //velocity.x = -velocity.x;
-              }
+          if let Some(ref mut velocity) = data.borrow::<VelocityComponent>(*e) {
               xv = velocity.x;
               yv = velocity.y;
           } else {
             return
           }
-          if let Some(ref mut position) = data.borrow::<Position>(*e) {
+          if let Some(ref mut position) = data.borrow::<PositionComponent>(*e) {
             position.x += xv * dt;
             position.y += yv * dt;
           }
@@ -142,13 +170,28 @@ impl BulkEntityProcess for MoveSystem {
   }
 }
 
-component!(Position {
+component!(PositionComponent {
     x: f64,
-    y: f64,
-    rad: f64
+    y: f64
 })
 
-component!(Velocity{
+#[deriving(Clone, PartialEq, Show)]
+pub enum Shape {
+    Point,
+    Circle(f64), // radius
+    Square(f64, f64), // width, height
+}
+
+impl Default for Shape {
+    fn default() -> Shape { Point }
+}
+
+component!(ShapeComponent {
+    shape: Shape,
+    border: Option<f64>
+})
+
+component!(VelocityComponent {
     x: f64,
     y: f64
 })
@@ -172,36 +215,50 @@ fn main() {
     );
 
     let mut world_builder = WorldBuilder::new();
-    world_builder.register_component::<Position>();
-    world_builder.register_component::<Velocity>();
+    world_builder.register_component::<PositionComponent>();
+    world_builder.register_component::<VelocityComponent>();
+    world_builder.register_component::<ShapeComponent>();
 
-    let aspect = Aspect::for_all(component_ids!(Position));
+    let aspect = Aspect::for_all(component_ids!(PositionComponent));
     let draw_sys = DrawSystem::new(ecs_event.clone());
     let sys = BulkEntitySystem::new(box draw_sys, aspect);
     world_builder.register_system(box sys);
 
-    let maspect = Aspect::for_all(component_ids!(Position, Velocity));
+    let maspect = Aspect::for_all(component_ids!(PositionComponent, VelocityComponent));
     let move_sys = MoveSystem::new(ecs_event.clone());
     let bsys = BulkEntitySystem::new(box move_sys, maspect);
     world_builder.register_system(box bsys);
 
     let ref mut world = world_builder.build();
 
-    let num_things: i32 = 80;
+    let num_things: i32 = 300;
 
     let ref mut rng = rand::task_rng();
     for _ in range(0, num_things) {
         let r = 30.0;
         let x = (800.0 - r) / 2.0;
         let y = (600.0 - r) / 2.0;
-        let d = Position { x: x, y: y, rad: r };
-        let m = Velocity {
+        let d = PositionComponent { x: x, y: y };
+        let m = VelocityComponent {
           x: rng.gen_range(-80.0, 80.0),
           y: rng.gen_range(-80.0, 80.0),
         };
-        world.build_entity(|c: &mut Components, e: Entity|{ 
+        let shape = ShapeComponent {
+          shape: if rng.gen() {
+                     Square(r, r)
+                 } else {
+                     Circle(r)
+                 },
+          border: if rng.gen() {
+                     Some(rng.gen_range(1.0, 5.0))
+                 } else {
+                     None
+                 }
+        };
+        world.build_entity(|c: &mut Components, e: Entity| {
             c.add(&e, d);
             c.add(&e, m);
+            c.add(&e, shape);
         });
     }
     let event_settings = EventSettings {
